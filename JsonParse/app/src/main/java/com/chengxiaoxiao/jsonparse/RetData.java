7ec0cec9943e65e5619882347166387067c1c08e
Cheng XiaoXiao <260714659@qq.com>
2016-09-20 20:57:50 +0800

    Revert "Revert "身份证信息查看器""
    This reverts commit fdfd58450a5eb8ddfca0e0b9d367f6ce7d708d9f.

---
+++ JsonParse/app/src/main/java/com/chengxiaoxiao/jsonparse/RetData.java
@@ -0,0 +1,42 @@
+package com.chengxiaoxiao.jsonparse;
+
+/**
+ * @author XiaoXiao
+ * @version $Rev$
+ */
+public class RetData
+{
+    private String address;
+    private String sex;
+    private String birthday;
+
+    public String getBirthday()
+    {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday)
+    {
+        this.birthday = birthday;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getSex()
+    {
+        return sex;
+    }
+
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+}
